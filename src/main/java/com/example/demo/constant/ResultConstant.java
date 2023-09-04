6aaed43c819478e75adb695d550b32d45d52f41c
lengx <1656629680@qq.com>
2023-09-04 09:48:46 +0800

    变更

--- src/main/java/com/example/demo/constant/ResultConstant.java
+++ src/main/java/com/example/demo/constant/ResultConstant.java
@@ -9,4 +9,8 @@ public class ResultConstant {
     public static final int USER_SUCCESS_MSG_CODE = 200;
 
     public static final String USER_SUCCESS_MSG = "用户请求成功";
+
+    public static final String USER_ID_ERROR_MSG_COD = "502";
+
+    public static final String USER_ID_ERROR_MSG = "用户身份信息校验失败";
 }
